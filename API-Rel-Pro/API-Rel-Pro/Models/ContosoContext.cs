bda0bd0c843eb9e15f0f539f1f6d3fcd9ba8f1f2
Carson <carsonjamescook@gmail.com>
2020-03-31 20:46:27 -0700

    Built initial unit test project
    Have one single dummy test to ensure that an in-memory database context is created.

---
+++ API-Rel-Pro/API-Rel-Pro/Models/ContosoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Rel_Pro.Models
+{
+    public class ContosoContext : DbContext
+    {
+        public ContosoContext()
+        {
+        }
+
+        public ContosoContext(DbContextOptions<ContosoContext> options) : base(options)
+        {
+        }
+    }
+}
