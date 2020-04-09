f4aea0b469d3fb8b4f82ad137b5d24f8b6aabae2
Carson <carsonjamescook@gmail.com>
2020-04-08 18:39:39 -0700

    Issue21: Built out "V1" directory and implemented all DTO classes for models

---
+++ API-ISD/API-ISD/V1/DTO/ContactV1DTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_ISD.V1.DTO
+{
+    public class ContactV1DTO
+    {
+        public int ContactID { get; set; }
+        public string ContactName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ API-ISD/API-ISD/V1/DTO/ServiceLanguageAssociationV1DTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_ISD.V1.DTO
+{
+    public class ServiceLanguageAssociationV1DTO
+    {
+        public int ServiceLanguageAssociation1 { get; set; }
+        public Nullable<int> ServiceID { get; set; }
+        public Nullable<int> LanguageID { get; set; }
+    }
+}
