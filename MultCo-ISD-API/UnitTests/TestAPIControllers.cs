8aeef613029b787969e4bbbfc69472579311cf30
BHolbs <blaineholbert@gmail.com>
2020-04-11 22:35:26 -0700

    Issue #29 : Add CRUD unit tests for Services Controller
    - Tests may not be very useful/meaningful w/out HTTP return codes, tests basically just check if something happened or not

--- MultCo-ISD-API/UnitTests/TestAPIControllers.cs
+++ MultCo-ISD-API/UnitTests/TestAPIControllers.cs
@@ -57,10 +57,174 @@ namespace UnitTests
                 {
                     context.Database.EnsureCreated();
                     context.Program.Add(new Program());
+                    context.Program.Add(new Program());
+                    context.Program.Add(new Program());
                     context.SaveChanges();
 
                     ProgramsController controller = new ProgramsController(context);
                     var actionResult = controller.GetProgram().Result;
+
+                    Assert.IsNotNull(actionResult);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        /* 
+         * Test Methods for Services Controller
+         *  NOTE: Cannot test failure w/out refactor to view a return code from Controller return type, this might make these tests not very helpful if we can't look at return codes
+         */
+        [TestMethod]
+        public void TestGetServices()
+        {
+            var connection = new SqliteConnection("Datasource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<InternalServicesDirectoryV1Context>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new InternalServicesDirectoryV1Context(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Service.Add(new Service());
+                    context.Service.Add(new Service());
+                    context.Service.Add(new Service());
+                    context.SaveChanges();
+
+                    ServicesController controller = new ServicesController(context);
+                    var actionResult = controller.GetService().Result;
+
+                    Assert.IsNotNull(actionResult);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [TestMethod]
+        public void TestGetService()
+        {
+            var connection = new SqliteConnection("Datasource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<InternalServicesDirectoryV1Context>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new InternalServicesDirectoryV1Context(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Service.Add(new Service());
+                    context.Service.Add(new Service());
+                    context.Service.Add(new Service());
+                    context.SaveChanges();
+
+                    ServicesController controller = new ServicesController(context);
+                    var actionResult = controller.GetService(2).Result;
+
+                    Assert.IsNotNull(actionResult);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [TestMethod]
+        public void TestPostService()
+        {
+            var connection = new SqliteConnection("Datasource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<InternalServicesDirectoryV1Context>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new InternalServicesDirectoryV1Context(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Service.Add(new Service());
+                    context.SaveChanges();
+
+                    var serv = new Service();
+                    ServicesController controller = new ServicesController(context);
+                    var actionResult = controller.PostService(serv).Result;
+
+                    Assert.IsNotNull(actionResult);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [TestMethod]
+        public void TestPutService()
+        {
+            var connection = new SqliteConnection("Datasource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<InternalServicesDirectoryV1Context>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new InternalServicesDirectoryV1Context(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Service.Add(new Service());
+                    context.SaveChanges();
+
+                    var serv = new Service { ServiceName = "Panda Adoption Society" };
+                    var controller = new ServicesController(context);
+                    var actionResult = controller.PutService(1, serv).Result;
+
+                    Assert.IsNotNull(actionResult);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [TestMethod]
+        public void TestDeleteService()
+        {
+            var connection = new SqliteConnection("Datasource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<InternalServicesDirectoryV1Context>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new InternalServicesDirectoryV1Context(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Service.Add(new Service());
+                    context.Service.Add(new Service());
+                    context.Service.Add(new Service());
+                    context.SaveChanges();
+
+                    var controller = new ServicesController(context);
+                    var actionResult = controller.DeleteService(2).Result;
 
                     Assert.IsNotNull(actionResult);
                 }
