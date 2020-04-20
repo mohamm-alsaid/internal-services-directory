7081c03d55e6cc0a6f42b9a103382ccfe8b4ebe8
Carson <carsonjamescook@gmail.com>
2020-04-19 18:14:10 -0700

    Issue #45: Implement authorization
    - Authorization is implemented (when using a preprocessor derective, `AUTH`)

--- MultCo-ISD-API/MultCo-ISD-API/Startup.cs
+++ MultCo-ISD-API/MultCo-ISD-API/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using IdentityServer4.AccessTokenValidation;
 using MultCo_ISD_API.Models;
 
 namespace MultCo_ISD_API
@@ -31,6 +34,37 @@ namespace MultCo_ISD_API
 			var connection = @"Server = localhost; Database = InternalServicesDirectoryV1; Trusted_Connection = True;";
 			services.AddDbContext<InternalServicesDirectoryV1Context>(options => options.UseSqlServer(connection));
 			services.AddControllers();
+#if AUTH
+			// JWT Token Auth with IdentityServer4
+			services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+				.AddIdentityServerAuthentication(o =>
+				{
+					o.Authority = Configuration["IdentityServerSettings:Uri"];
+					o.ApiName = Configuration["IdentityServerSettings:Scope"];
+					o.RequireHttpsMetadata = true;
+					o.EnableCaching = true;
+					o.CacheDuration = TimeSpan.FromMinutes(30);
+				});
+
+
+			// Example Scope Policies with a simple read or write
+			services.AddAuthorization(o =>
+			{
+				o.AddPolicy("Writer", p =>
+				{
+					p.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+					p.RequireAuthenticatedUser();
+					p.RequireScope("mult-co-isd-api.write");
+				});
+				o.AddPolicy("Reader", p =>
+				{
+					p.AuthenticationSchemes.Add(JwtBearerDefaults.AuthenticationScheme);
+					p.RequireAuthenticatedUser();
+					p.RequireScope("mult-co-isd-api.read");
+				});
+			});
+#endif
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
