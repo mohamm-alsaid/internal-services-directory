95bd037ef546963bd57aa54018b4c48615ed3c9f
Gubriels <gabrieledwards@ymail.com>
2020-06-10 23:14:54 -0700

    Issue124: Update documentation language

--- MultCo-ISD-API/MultCo-ISD-API/V1/Controllers/ServiceController.cs
+++ MultCo-ISD-API/MultCo-ISD-API/V1/Controllers/ServiceController.cs
@@ -79,9 +79,7 @@ namespace MultCo_ISD_API.V1.Controllers
         /// <summary>
         /// Get a particular service by service ID.
         /// </summary>
-        /// <remarks>
-        /// Returns the service with matching id, get all information associated with it
-        /// </remarks>
+
         [SwaggerOperation(Tags = new[] { "Reader" })]
 
 		// GET: api/Services/5
@@ -111,10 +109,10 @@ namespace MultCo_ISD_API.V1.Controllers
 		}
     
         /// <summary>
-        /// Get all services by language 
+        /// Get services by language(s)
         /// </summary>
         /// <remarks>
-        /// Returns all services with a matching language 
+        /// Multiple languages can be specified. Example: 'german,spanish' returns all services associated with either language
         /// </remarks>
         /// <param name="lang"></param>
         /// <returns></returns>
@@ -225,7 +223,7 @@ namespace MultCo_ISD_API.V1.Controllers
 			}
 		}
 		/// <summary>
-		/// Get all services with a building Id
+		/// Get services by building Id
 		/// </summary>
 		/// <param name="buildingId"></param>
 		/// <returns></returns>
@@ -276,7 +274,7 @@ namespace MultCo_ISD_API.V1.Controllers
 		}
 
 		/// <summary>
-		/// Get all services with a program Id
+		/// Get services by program Id
 		/// </summary>
 		/// <param name="programId"></param>
 		/// <returns></returns>
@@ -308,10 +306,10 @@ namespace MultCo_ISD_API.V1.Controllers
 			return Ok(serviceDTOs);
 		}
 		/// <summary>
-		/// Get all services with a division and or department Id
+		/// Get services by division and or department Id
 		/// </summary>
 		/// <remarks>
-		/// paginated
+		/// Results are paginated
 		/// </remarks>
 		[SwaggerOperation(Tags = new[] { "Reader" })]
 
@@ -361,11 +359,8 @@ namespace MultCo_ISD_API.V1.Controllers
 		}
 
 		/// <summary>
-		/// Get all services with a matching name ( even if the name is a substring of another )
+		/// Get services by a partial or fully matching service name
 		/// </summary>
-		/// <remarks>
-		/// matching the regex: .*a_string.* or SQL: like %a_string%
-		/// </remarks>
 		[SwaggerOperation(Tags = new[] { "Reader" })]
 
 		//GET: api/Services/Name
@@ -394,7 +389,7 @@ namespace MultCo_ISD_API.V1.Controllers
 		/// Update a service with a certain ID
 		/// </summary>
 		/// <remarks>
-		/// Add a service if it doesn't already exist and update a service if it exist
+		/// Add a service if it doesn't already exist and update a service if it already exists
 		/// </remarks>
 		[SwaggerOperation(Tags = new[] { "Writer" })]
 
@@ -429,11 +424,8 @@ namespace MultCo_ISD_API.V1.Controllers
 			}
 		}
 		/// <summary>
-		/// Post a new service
+		/// Create a new service
 		/// </summary>
-		/// <remarks>
-		/// Adds a service
-		/// </remarks>
 		[SwaggerOperation(Tags = new[] { "Writer" })]
 
 		// POST: api/Services
